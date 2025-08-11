b38b53e0045515a8a534a18624bb0ad90849b4ef
khaled hussien <anakhaled209@gmail.com>
2025-08-11 10:49:44 +0300

    Initial Commit

---
+++ TEA-Movie/TEA_MovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  TEA_MovieApp.swift
+//  TEA-Movie
+//
+//  Created by KhaleD HuSsien on 11/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct TEA_MovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
